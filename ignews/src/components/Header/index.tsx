1b7734ba05ac6889fc93821a1e78bb28425cb838
Caio Detore <caio.detore@aswti.com.br>
2022-02-10 17:46:17 -0300

    ignews (nextjs) e início dashgo (chakra-ui)

---
+++ ignews/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import styles from './styles.module.scss'
+import logoSvg from '../../../public/images/logo.svg'
+import { SignInButton } from "../SignInButton";
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Image src={logoSvg} alt="ig.news" />
+                <nav>
+                    <a href="#" className={styles.active}> Home </a>
+                    <a href="#"> Post </a>
+                </nav>
+
+                <SignInButton />
+            </div>
+        </header>
+    )
+}
---
+++ ignews/src/components/SignInButton/index.tsx
@@ -0,0 +1,32 @@
+import styles from './styles.module.scss'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from 'next-auth/react'
+
+
+export function SignInButton() {
+    const {data:session} = useSession()
+
+    console.log(session);
+
+    return session ? (
+        <button 
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signOut()}
+        >
+            <FaGithub color="#84c561" />
+            {session.user.name}
+            <FiX color="#737386" className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button 
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
+        >
+            <FaGithub color="#eba417" />
+            Sign in with Github
+        </button>
+    );
+}
