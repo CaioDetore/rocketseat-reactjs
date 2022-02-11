e17f62d581ee6914dc31c4a3f6f2515f2bcf5430
Caio Detore <caio.detore@aswti.com.br>
2022-02-11 17:59:02 -0300

    dashgo: responsividade e finalização

---
+++ dashgo/src/components/Sidebar/SidebarNav.tsx
@@ -0,0 +1,20 @@
+import { Stack } from "@chakra-ui/react";
+import { RiContactsLine, RiDashboardLine, RiGitMergeFill, RiInputMethodLine } from "react-icons/ri";
+import { NavLink } from "./NavLink";
+import { NavSection } from "./NavSection";
+
+export function SidebarNav() {
+    return (
+        <Stack spacing="12" align="flex-start">
+            <NavSection title="GERAL">
+                <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
+                <NavLink icon={RiContactsLine} href="/users">Usuários</NavLink>
+            </NavSection>
+
+            <NavSection title="AUTOMAÇÃO">
+                <NavLink icon={RiInputMethodLine} href="#">Formulários</NavLink>
+                <NavLink icon={RiGitMergeFill} href="#">Automação</NavLink>
+            </NavSection>
+        </Stack>
+    )
+}
--- dashgo/src/pages/index.tsx
+++ dashgo/src/pages/index.tsx
@@ -1,7 +1,33 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
 import { Input } from '../components/Form/Input'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+})
 
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const errors = formState.errors
+  console.log(errors)
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    
+    console.log(values)
+  }
+
   return (
     <Flex 
       w="100vw" 
@@ -9,7 +35,7 @@ export default function SignIn() {
       align="center" 
       justify="center"
     >
-      <Flex 
+      <Flex
         as="form"
         width="100%"
         maxWidth={360}
@@ -17,14 +43,36 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
-      > 
+        onSubmit={handleSubmit(handleSignIn)}
+      >
         <Stack spacing="4">
-          <Input name='email' type='email' label='E-mail' placeholder='exemple@email.com'/>
-          <Input name='password' type='password' label='Senha' placeholder='*****'/>
-          
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            placeholder="exemple@email.com"
+            error={errors.email}
+            {...register("email")}
+          />
+
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            placeholder="*****"
+            error={errors.password}
+            {...register("password")}
+          />
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink">Entrar </Button>
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="pink"
+          isLoading={formState.isSubmitting}
+        >
+          Entrar{" "}
+        </Button>
       </Flex>
     </Flex>
-  )
+  );
 }
